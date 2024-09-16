99cb233c61885b1bce43d9f7fff6d2fd84c3b6b1
01fe22bca202 <156280147+RamyaB-202@users.noreply.github.com>
2024-09-16 21:27:11 +0530

    Update MyFirstJDBCProgram.java

--- MyFirstJDBCProgram.java
+++ MyFirstJDBCProgram.java
@@ -12,7 +12,7 @@ public class MyFirstJDBCProgram {
             //step 2: Open DB connection
         String URL = "jdbc:oracle:thin:@localhost:1522:xe";
         String USER = "system";
-        String PASSWORD = "ramz0dbt2orac";
+        String PASSWORD = "********";
          con = DriverManager.getConnection(URL,USER,PASSWORD);
             if(con==null){
                 System.out.println("Connection Unsuccessful");
@@ -20,34 +20,34 @@ public class MyFirstJDBCProgram {
                 System.out.println("Connection Successful");
             }
             //step 3: Create statement
-        Statement stmt = con.createStatement();
-       // String createquery = "create table Student(sid int primary key, sname varchar(20), city varchar(20))"; 
-       // int output = stmt.executeUpdate(createquery);
-        // String pstmtInsertSql = "insert into  student values(?,?,?)";
-        // PreparedStatement pstmt =  con.prepareStatement(pstmtInsertSql);
-        // pstmt.setInt(1,16);
-        // pstmt.setString(2,"vivek");
-        // pstmt.setString(3,"Delhi");
+         Statement stmt = con.createStatement();
+         String createquery = "create table Student(sid int primary key, sname varchar(20), city varchar(20))"; 
+         int output = stmt.executeUpdate(createquery);
+         String pstmtInsertSql = "insert into  student values(?,?,?)"; // Prepared Statement Insert Query
+         PreparedStatement pstmt =  con.prepareStatement(pstmtInsertSql);
+         pstmt.setInt(1,16);
+         pstmt.setString(2,"vivek");
+         pstmt.setString(3,"Delhi");
         //Step 4: Execute statement
-        // int r = pstmt.executeUpdate();
-        // System.out.println(r);
-        //int output = stmt.executeUpdate(query);
-        //String insertquery = "insert into Student values(1,'Ram','hubli')"; // Rishab
-       // String insertquery = "insert into Student values(2,'Sita','hubli')"; // Akash
-        //int output = stmt.executeUpdate(insertquery);
+         int r = pstmt.executeUpdate();
+         System.out.println(r);
+         //int output = stmt.executeUpdate(query);
+         //String insertquery = "insert into Student values(1,'Ram','hubli')"; // Rishab
+         String insertquery = "insert into Student values(2,'Sita','hubli')"; // Akash
+         int output = stmt.executeUpdate(insertquery);
         String selectquery = "select * from Student";
-        //int output = stmt.executeUpdate(selectquery);
-        //boolean success = stmt.execute(selectquery);
-        ResultSet rs = stmt.executeQuery(selectquery);
-       // System.out.println(output);
+        //int output = stmt.executeUpdate(selectquery); // OR
+        // System.out.println(output);
+        //boolean success = stmt.execute(selectquery);  // OR
         //System.out.println(success);
+        ResultSet rs = stmt.executeQuery(selectquery);  // Scrollable Statement Select Query
         while (rs.next()){
            System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
            System.out.println(rs.getInt("sid") + " " + rs.getString("sname") + " " + rs.getString("city"));
        }
         //Step 5: Close connection
-       // System.out.println("Connection closed");
-        //con.close();
+        System.out.println("Connection closed");
+        con.close();
         } catch (ClassNotFoundException e) {
         } catch (SQLException e) {
             e.printStackTrace();
